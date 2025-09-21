fcb3b4f0b0265058f3b89d4975eeffa37da6e86c
Kan Kawabata <kkawabat@asu.edu>
2025-09-21 14:20:21 -0700

    fix a binding issue

--- signaling-server/src/server.ts
+++ signaling-server/src/server.ts
@@ -27,7 +27,7 @@ export class SignalingServer {
       this.handleConnection(ws);
     });
 
-    server.listen(port, () => {
+    server.listen(port, '0.0.0.0', () => {
       console.log(`Signaling server running on port ${port}`);
     });
 
